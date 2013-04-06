feb0bf02a32d25716fff335ad5ac67d098e0093d
navaneethrameshan <voldermot_88>
2013-04-07 01:21:46 +0200

    added support for python memory profiler

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from server.couchbase import main   
 import os
 
+def launch_memory_usage_server(port = 8088):
+    import cherrypy
+    import dowser
+ 
+    cherrypy.tree.mount(dowser.Root())
+    cherrypy.config.update({
+        'environment': 'embedded',
+        'server.socket_port': port
+    })
+   
+    cherrypy.server.socket_host = '147.83.35.241'
+    cherrypy.engine.start()
+
 path= os.path.join(os.path.dirname(__file__), 'node_list_shelf.db')
 if(os.path.exists(path)):
     os.remove(path)
+
+launch_memory_usage_server()
 main.main()
