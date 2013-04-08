c2016e63c8de864be375dc781225c16f5b5ad4db
navaneethrameshan <voldermot_88>
2013-04-08 20:55:11 +0200

    following convention

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ def launch_memory_usage_server(port = 8088):
     cherrypy.server.socket_host = '147.83.35.241'
     cherrypy.engine.start()
 
-path= os.path.join(os.path.dirname(__file__), 'node_list_shelf.db')
-if(os.path.exists(path)):
-    os.remove(path)
 
-launch_memory_usage_server()
-main.main()
+if __name__ =='__main__':
+    path= os.path.join(os.path.dirname(__file__), 'node_list_shelf.db')
+    if(os.path.exists(path)):
+        os.remove(path)
+
+    launch_memory_usage_server()
+    main.main()
